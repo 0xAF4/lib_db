40db6c66c0045fc75527d43757076cdefc0e8ea7
0xAF4 <karim.badalov1612@gmail.com>
2023-11-18 21:55:07 +0600

    PostgreSQL

--- postgresql.go
+++ postgresql.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"time"
 
+	"github.com/jackc/pgconn"
 	"github.com/jackc/pgx/v4"
 )
 
@@ -53,17 +54,144 @@ func (d *DB_PostgreSQL) Close() {
 }
 
 func (d *DB_PostgreSQL) Exec(txType int, query string, args ...interface{}) (*string, error) {
-	return nil, nil
+	if d.db[txType] == nil {
+		if err := d.Open(); err != nil {
+			return nil, err
+		}
+	}
+
+	coma, err := d.db[txType].Exec(context.Background(), query, args...)
+	str := coma.String()
+	return &str, err
 }
 
 func (d *DB_PostgreSQL) ExecWithTimeout(txType int, timeOut time.Duration, query string, args ...interface{}) (*string, error) {
-	return nil, nil
+	if d.db[txType] == nil {
+		if err := d.Open(); err != nil {
+			return nil, err
+		}
+	}
+
+	ctx := context.Background()
+	ctxTime, cancel := context.WithTimeout(ctx, timeOut)
+	defer cancel()
+
+	tx, err := d.db[txType].Begin(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	var rows pgconn.CommandTag
+	done := make(chan bool)
+
+	go func() {
+		rows, err = tx.Exec(ctxTime, query, args...)
+		done <- true
+	}()
+
+	select {
+	case <-ctxTime.Done():
+		tx.Rollback(ctx)
+		return nil, ctxTime.Err()
+	case <-done:
+		if err != nil {
+			tx.Rollback(ctx)
+			return nil, err
+		} else {
+			res := rows.String()
+			if err != nil {
+				tx.Rollback(ctx)
+				return nil, err
+			}
+			tx.Commit(ctx)
+			return &res, nil
+		}
+	}
 }
 
 func (d *DB_PostgreSQL) QueryRow(txType int, query string, args ...interface{}) (*DBResult, error) {
-	return nil, nil
+	if d.db[txType] == nil {
+		if err := d.Open(); err != nil {
+			return nil, err
+		}
+	}
+
+	rows, err := d.db[txType].Query(context.Background(), query, args...)
+	if err != nil {
+		return nil, err
+	}
+	return d.rowsToMap(rows)
 }
 
 func (d *DB_PostgreSQL) QueryRowWithTimeout(txType int, timeOut time.Duration, query string, args ...interface{}) (*DBResult, error) {
-	return nil, nil
+	if d.db[txType] == nil {
+		if err := d.Open(); err != nil {
+			return nil, err
+		}
+	}
+
+	ctx := context.Background()
+	ctxTime, cancel := context.WithTimeout(ctx, timeOut)
+	defer cancel()
+
+	tx, err := d.db[txType].Begin(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	var rows pgx.Rows
+	done := make(chan bool)
+
+	go func() {
+		rows, err = tx.Query(ctxTime, query, args...)
+		done <- true
+	}()
+
+	select {
+	case <-ctxTime.Done():
+		rows.Close()
+		tx.Rollback(ctx)
+		return nil, ctxTime.Err()
+	case <-done:
+		if err != nil {
+			rows.Close()
+			tx.Rollback(ctx)
+			return nil, err
+		} else {
+			res, err := d.rowsToMap(rows)
+			defer rows.Close()
+			if err != nil {
+				tx.Rollback(ctx)
+				return nil, err
+			}
+			tx.Commit(ctx)
+			return res, nil
+		}
+	}
+}
+
+func (d *DB_PostgreSQL) rowsToMap(rows pgx.Rows) (*DBResult, error) {
+	columns := rows.FieldDescriptions()
+	var result DBResult
+
+	values := make([]interface{}, len(columns))
+	for i := range values {
+		values[i] = new(interface{})
+	}
+
+	for rows.Next() {
+		err := rows.Scan(values...)
+		if err != nil {
+			return nil, err
+		}
+
+		rowMap := make(map[string]interface{})
+		for i, column := range columns {
+			rowMap[string(column.Name)] = *(values[i].(*interface{}))
+		}
+
+		result = append(result, rowMap)
+	}
+
+	return &result, nil
 }
